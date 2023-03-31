b53c25ea06f1751f59d6d63dc222e6b850a0591d
Antônio Gabriel de Matos <antoniobarbarian@gmail.com>
2023-03-31 20:10:24 -0300

    Exercício 2

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,14 +1,6 @@
-/**
- * Cadastre as rotas da sua aplicação!
- * Lembrando: a estrutura do objeto que configura a rota é a seguinte:
- *
- * {
- *   path: "/caminho-da-rota",
- *   element: <ComponenteDaRota />,
- * }
- */
-
 import { createBrowserRouter } from "react-router-dom";
+import { QuemSou } from "./pages/QuemSou";
+import { Home } from "./pages/Home";
 import { App } from "./App";
 
 export const appRouter = createBrowserRouter([
@@ -16,7 +8,8 @@ export const appRouter = createBrowserRouter([
     path: "/",
     element: <App />,
     children: [
-      //  as rotas serão incluídas aqui!
+      { path: "/home", element: <Home /> },
+      { path: "/quem-sou", element: <QuemSou /> },
     ],
   },
 ]);
